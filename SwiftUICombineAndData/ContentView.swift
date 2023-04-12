38e02881bbd25d97f3588ef54a12569de7379ed4
Farangis <Farangis>
2023-04-12 14:51:05 +0500

    first commit

--- SwiftUICombineAndData/ContentView.swift
+++ SwiftUICombineAndData/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var contentOffset = CGFloat(0)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack(alignment: .top) {
+                TrackableScrollView(offsetChanged: { offset in
+                    contentOffset = offset.y
+//                    print("contentOffset", contentOffset)
+                }) {
+                    Text("Hello, world!")
+                        .padding()
+                }
+
+                VisualEffectBlur(blurStyle: .systemMaterial)
+                    .opacity(contentOffset < -16 ? 1 : 0)
+                    .animation(.easeIn)
+                    .ignoresSafeArea()
+                    .frame(height: 0)
+            }
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(AccountBackground())
+            .navigationBarHidden(true)
         }
-        .padding()
+        
+        
     }
 }
 
